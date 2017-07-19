54bb3bb778f6fb016bf2872685a1229008150f2c
Edoardo Branchi <edoardo.branchi@stud.unifi.it>
2017-07-19 12:08:45 +0200

    Ora funziona anche nel caso due valori siano uguali e addirittura va bene anche la precisione di macchina che tronca a 0.000001

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ struct list{
 void init(struct list **ptrptr);
 void ord_insert(struct list ** ptrptr,float value);
 void merge(struct list **ptrptr, struct list **ptr);
-boolean is_lower(float a, float b);
+boolean is_lower_or_equal(float a, float b);
 void visit(struct list* ptr);
 void pre_insert(struct list ** ptrptr, float value);
 
@@ -61,24 +61,21 @@ void merge(struct list **ptrptr, struct list **ptr) {
             (*ptrptr)->next_ptr = *ptr;
             break;
         }
-        else if(is_lower((*ptrptr)->value,(*ptr)->value )== TRUE && is_lower((*ptr)->value,tmpA->value)== TRUE ) {
+        else if(is_lower_or_equal((*ptrptr)->value,(*ptr)->value )== TRUE && is_lower_or_equal((*ptr)->value,tmpA->value)== TRUE ) {
             (*ptrptr)->next_ptr=*ptr;
             *ptr=(*ptr)->next_ptr;
             (*ptrptr)->next_ptr->next_ptr=tmpA;
+            ptrptr = &(*ptrptr)->next_ptr;
 
         }else{
             ptrptr = &(*ptrptr)->next_ptr;
             tmpA=(*ptrptr)->next_ptr;
         }
-        /*if((*ptrptr)->next_ptr == NULL && (*ptr)->next_ptr != NULL){
-            (*ptrptr)->next_ptr = *ptr;
-            *ptr = (*ptr)->next_ptr;
-        }*/
     }
 }
 
-boolean is_lower(float a, float b){
-    if(a - b < FLT_EPSILON) {
+boolean is_lower_or_equal(float a, float b){
+    if(a-b<FLT_EPSILON || (a - b < FLT_EPSILON*a && -FLT_EPSILON*a < (a-b) )) {
         return TRUE;
     }else return FALSE;
 }
