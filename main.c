1af8c3fe4e4e639a083e258e86485e55c99381f7
Edoardo Branchi <edoardo.branchi@stud.unifi.it>
2017-07-19 11:29:27 +0200

    Ora funziona per bene anche se fa un po schifo

--- main.c
+++ main.c
@@ -13,8 +13,8 @@ struct list{
 
 void init(struct list **ptrptr);
 void ord_insert(struct list ** ptrptr,float value);
-void merge(struct list **ptrptr, struct list *ptr);
-boolean is_lower(struct list a, struct list b);
+void merge(struct list **ptrptr, struct list **ptr);
+boolean is_lower(float a, float b);
 void visit(struct list* ptr);
 void pre_insert(struct list ** ptrptr, float value);
 
@@ -41,9 +41,9 @@ int main() {
     for(b=0;b<i;b++){
         printf("Inserisci il valore %d ", b);
         scanf("%f",&values);
-        ord_insert(&A,values);
+        ord_insert(&B,values);
     }
-    merge(&A,B);
+    merge(&A,&B);
     visit(A);
 }
 
@@ -52,29 +52,33 @@ void init(struct list **ptrptr){
     *ptrptr=NULL;
 }
 
-void merge(struct list **ptrptr, struct list *ptr) {
-    while(ptr != NULL){
-        if(is_lower(**ptrptr, *ptr) == TRUE ) {
-            (*ptrptr) = (*ptrptr)->next_ptr;
-        }else{
-            struct list * tmp;
-            *tmp = **ptrptr;
-            *ptrptr = (*ptrptr)->next_ptr;
-            tmp->next_ptr = ptr;
-            tmp = ptr;
-            ptr = ptr->next_ptr;
-            tmp->next_ptr = (*ptrptr);
-        }
-        if((*ptrptr) == NULL && ptr != NULL){
-            (*ptrptr)->next_ptr = ptr;
-            ptr = ptr->next_ptr;
-        }
-    }
+void merge(struct list **ptrptr, struct list **ptr) {
+    struct list * tmpA;
+    tmpA=(*ptrptr)->next_ptr;
 
+    while(*ptr != NULL && *ptrptr != NULL){
+        if((*ptrptr)->next_ptr == NULL){
+            (*ptrptr)->next_ptr = *ptr;
+            break;
+        }
+        else if(is_lower((*ptrptr)->value,(*ptr)->value )== TRUE && is_lower((*ptr)->value,tmpA->value)== TRUE ) {
+            (*ptrptr)->next_ptr=*ptr;
+            *ptr=(*ptr)->next_ptr;
+            (*ptrptr)->next_ptr->next_ptr=tmpA;
+
+        }else{
+            ptrptr = &(*ptrptr)->next_ptr;
+            tmpA=(*ptrptr)->next_ptr;
+        }
+        /*if((*ptrptr)->next_ptr == NULL && (*ptr)->next_ptr != NULL){
+            (*ptrptr)->next_ptr = *ptr;
+            *ptr = (*ptr)->next_ptr;
+        }*/
+    }
 }
 
-boolean is_lower(struct list a, struct list b){
-    if(a.value - b.value < FLT_EPSILON) {
+boolean is_lower(float a, float b){
+    if(a - b < FLT_EPSILON) {
         return TRUE;
     }else return FALSE;
 }
